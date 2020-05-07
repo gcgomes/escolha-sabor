08eb46e7099d20898dd84c0f379c952d26c77674
Gabriel Gomes <gabrielcesariogomes@gmail.com>
2020-05-07 19:24:31 -0300

    Animação de switch, lógica de estados e modificação de funções

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Switch from './components/Switch';
+
+const initialState = {
+  pepperoni: false,
+  portuguesa: false,
+  marguerita: false
+};
 
 function App() {
+  const [switches, setSwitches] = useState(initialState);
+
+  function toggleSwitch(id) {
+    const currentState = switches[id];
+    if (currentState === undefined) return;
+    const activeSwitches = Object.values(switches).filter(value => value).length;
+    const MAX_ACTIVE = Object.values(switches).length - 1;
+    const newState = (activeSwitches === MAX_ACTIVE) ? initialState : switches;
+    setSwitches({...newState, [id]: !currentState})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"}>
+      <h1>Escolha dois sabores</h1>
+      <Switch onClick={toggleSwitch} id={"pepperoni"} active={switches['pepperoni']} label={"Pepperoni"} />
+      <Switch onClick={toggleSwitch} id={"portuguesa"} active={switches['portuguesa']} label={"Portuguesa"} />
+      <Switch onClick={toggleSwitch} id={"marguerita"} active={switches['marguerita']} label={"Marguerita"} />
     </div>
   );
 }
